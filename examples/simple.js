993c72595df5d6d9992e764fedcd37d73ec35ba3
DaAwesomeP <DaAwesomeP@users.noreply.github.com>
2015-09-03 17:43:21 -0500

    Inital commit

---
+++ examples/simple.js
@@ -0,0 +1,20 @@
+
+var session = require('./');
+var koa = require('koa');
+var app = koa();
+
+app.keys = ['some secret hurr'];
+
+app.use(session());
+
+app.use(function(next){
+  return function *(){
+    if ('/favicon.ico' == this.path) return;
+    var n = this.session.views || 0;
+    this.session.views = ++n;
+    this.body = n + ' views';
+  }
+})
+
+app.listen(3000);
+console.log('listening on port 3000');
